33517de93582ac0a18859f5a55bfea4a56880a29
Mahesh <mahesheu@gmail.com>
2016-03-13 20:41:22 +0530

    form validation

---
+++ form-validation/app.js
@@ -0,0 +1,45 @@
+angular.module('angular-validator-demo', ['angularValidator']);
+angular.module('angular-validator-demo').controller('MyCtrl',function($scope){
+
+	$scope.submitMyForm = function(){
+		console.log($scope.form);
+		
+		
+	};
+
+	$scope.myCustomValidator = function(text){		
+		return true;
+	};
+
+
+	$scope.anotherCustomValidator = function(text){
+		if(text === "rainbow"){
+			return true;
+		}
+		else return "type in 'rainbow'";
+	};
+
+	$scope.passwordValidator = function(password) {
+
+		if(!password){return;}
+		
+		if (password.length < 6) {
+			return "Password must be at least " + 6 + " characters long";
+		}
+
+		if (!password.match(/[A-Z]/)) {
+			 return "Password must have at least one capital letter";
+		}
+
+		if (!password.match(/[0-9]/)) {
+			 return "Password must have at least one number";
+		}
+
+		return true;
+	};
+
+
+
+});
+		
+		
